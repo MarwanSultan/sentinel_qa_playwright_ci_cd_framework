148458fe2911b4d8695546d4be70c991b83efc8e
Marwan <marwan.sultan@gmail.com>
2025-12-26 10:14:18 -0500

    Updated product search test.

--- tests/product_search_test.spec.ts
+++ tests/product_search_test.spec.ts
@@ -3,9 +3,10 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   //Validate search functionality.
 
-  await page.goto('https://www.amazon.com/');
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
-  await page.getByRole('searchbox', { name: 'Search Amazon' }).fill('Samsung TV');
-  await page.getByRole('button', { name: 'Go', exact: true }).click();
-  await expect(page.getByRole('heading', { name: 'Results', exact: true })).toBeVisible();
+await page.goto('https://www.amazon.com', { waitUntil: 'domcontentloaded' });
+
+const searchBox = page.locator('#twotabsearchtextbox');
+await expect(searchBox).toBeVisible({ timeout: 15000 });
+await searchBox.fill('Samsung TV');
+
 });
